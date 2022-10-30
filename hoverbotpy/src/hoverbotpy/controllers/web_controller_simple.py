a2b17bc539e0b355cb1cad2bc12c1ac08ef86761
Devlin Ih <dih@olin.edu>
2022-10-30 15:36:40 -0400

    Add new drivers and controller

---
+++ hoverbotpy/src/hoverbotpy/controllers/web_controller_simple.py
@@ -0,0 +1,136 @@
+from hoverbotpy.drivers.pi_pico_simple import SimpleFan
+from ctypes import set_errno
+from time import time
+import asyncio
+
+import tornado.web
+import tracemalloc
+
+tracemalloc.start()
+
+last_hover = 0
+last_forward = 0
+last_right = 0
+last_left = 0
+forward_speed = 0
+steer = 0
+
+
+class Hover(tornado.web.RequestHandler):
+    def get(self):
+        global last_hover
+        print("hover click")
+        last_hover = time()
+
+
+class Estop(tornado.web.RequestHandler):
+    def get(self):
+        global driver
+        global steer
+        global forward_speed
+        steer = 0
+        forward_speed = 0
+        driver.stop()
+        print("ESTOP ESTOP ESTOP")
+
+
+class Forward(tornado.web.RequestHandler):
+    def get(self):
+        global last_forward
+
+        global driver
+        global forward_speed
+        if forward_speed <= 90:
+            forward_speed += 10
+        driver.set_forward_speed(forward_speed)
+        print("forward click")
+        print(forward_speed)
+        last_forward = time()
+
+
+class Reverse(tornado.web.RequestHandler):
+    def get(self):
+        global last_forward
+        global driver
+        global forward_speed
+        if forward_speed >= 10:
+            forward_speed -= 10
+        driver.set_forward_speed(forward_speed)
+        print("rev click")
+        print(forward_speed)
+        last_forward = time()
+
+
+class Right(tornado.web.RequestHandler):
+    def get(self):
+        global last_right
+        global driver
+        global steer
+        if steer >= -.5:
+            steer -= .5
+        driver.set_steering_angle(steer)
+        print("right click")
+        print(steer)
+        last_right = time()
+
+
+class Left(tornado.web.RequestHandler):
+    def get(self):
+        global last_left
+        global steer
+        if steer <= .5:
+            steer += .5
+        driver.set_steering_angle(steer)
+        print("left click")
+        print(steer)
+        last_left = time()
+
+
+class Index(tornado.web.RequestHandler):
+    def get(self):
+        #self.write("Hello, world")
+        self.render("web_controller.html")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/darkmode.css", tornado.web.StaticFileHandler,
+         {"path": "darkmode.css"},),
+        (r"/", Index),
+        (r"/hover/", Hover),
+        (r"/0_pressed/", Estop),
+        (r"/estop/", Estop),
+        (r"/forward/", Forward),
+        (r"/w_pressed/", Forward),
+        (r"/s_pressed/", Reverse),
+        # there will be no half a pressed with this code
+        (r"/a_pressed/", Left),
+        (r"/d_pressed/", Right),
+        #(r"/h_pressed/", HoverToggle),
+    ], debug=True)
+
+# async def
+
+
+async def app_start():
+    app = make_app()
+    app.listen(8888)
+    await asyncio.Event().wait()
+
+
+async def web_app():
+    print("web server start")
+    app = make_app()
+    app.listen(8888)
+
+
+async def main():
+    await app_start()
+    # asyncio.create_task(app_start())
+    while (1):
+        print(last_forward)
+
+if __name__ == "__main__":
+    driver = SimpleFan()
+    asyncio.run(main())
+    # main()
---
+++ hoverbotpy/src/hoverbotpy/drivers/pi_pico_simple.py
@@ -0,0 +1,75 @@
+from hoverbotpy.drivers.driver_abc import HovercraftDriver
+from hoverbotpy.drivers.pin_defs import *
+from gpiozero import Servo
+from time import sleep
+import serial
+
+PWM_FREQ = 15000
+SERVO_DIST = 85
+SERVO_MID = 90
+
+
+class SimpleFan(HovercraftDriver):
+    ''''''
+
+    def __init__(self):
+        if SERVOPIN < 2 or \
+           SERVOPIN > 27:
+            raise Exception(
+                "All pin definitions must match pins on the raspberry pi")
+        self.pico = serial.Serial(
+            port='/dev/ttyACM0', baudrate=115200, timeout=.1)
+        self.steer_motor = Servo(SERVOPIN)
+        #self.hover = 0
+        self.forward = 0
+        self.steering = 0
+        sleep(0.1)
+        self.steer_motor.min()  # wig wag the servo to show its been started up
+        sleep(0.2)
+        self.steer_motor.max()
+        sleep(0.2)
+        self.steer_motor.min()
+        sleep(0.2)
+        self.steer_motor.max()
+        sleep(0.2)
+        self.steer_motor.mid()
+        pass
+
+    def set_hover_speed(self, speed):
+        '''sets the speed of the hover motor
+        args: 
+            speed: a number of the speed of the motor(0 to 100)'''
+        # self.hover = (speed/50)-1  # this takes a 1 to -1
+        # self.hover_motor.value(self.hover)
+        pass
+
+    def set_forward_speed(self, speed):
+        '''sets the speed of the forward motor
+        args: 
+            speed: a number of the speed of the motor(0 to 100)'''
+        self.forward = (speed/50)-1
+        self.pico.write(bytes("f"+str(speed), 'utf-8'))
+
+        pass
+
+    def set_steering_angle(self, angle):
+        '''sets the angle of the steering servo motor
+        args: 
+            speed: a number of the angle to (-1 to 1)'''
+        #self.steering = angle
+        # self.steer_motor.value=angle
+        self.steering = angle
+        self.pico.write(bytes("s"+str(angle*SERVO_DIST+SERVO_MID), 'utf-8'))
+        pass
+
+    def stop(self):
+        '''turn off all motors and return steer to mid'''
+        # self.forward_motor.detach()
+        # self.hover_motor.detach()
+        self.pico.write(bytes("s"+str(SERVO_MID), 'utf-8'))
+        self.pico.write(bytes("f"+str(0), 'utf-8'))
+        #self.hover = 0
+        self.forward = 0
+        self.steering = 0
+        # gp.cleanup()
+        pass
