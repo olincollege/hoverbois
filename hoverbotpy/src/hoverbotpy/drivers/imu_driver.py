e735d83a6dbe1cd703984f9732c41ea512ef65e1
hurricaneJoef <hurricane.joef@gmail.com>
2022-11-04 21:43:10 -0400

    fixed new code

--- hoverbotpy/src/hoverbotpy/drivers/imu_driver.py
+++ hoverbotpy/src/hoverbotpy/drivers/imu_driver.py
@@ -1,6 +1,14 @@
 import pigpio
 from threading import Thread, Lock
 
+DEFAULT_REQUESTS = [
+            #"X_POS","Y_POS","Z_POS",
+            #"X_VEL","Y_VEL","Z_VEL",
+            "X_ACC","Y_ACC","Z_ACC",
+            #"X_DEG","Y_DEG","Z_DEG",
+            "X_DPS","Y_DPS","Z_DPS",
+            "X_MAG","Y_MAG","Z_MAG"
+            ]
 
 class corrected_imu():
     ''' this is the position corrected imu for com of object'''
@@ -10,14 +18,6 @@ class corrected_imu():
         self.mag_adr = mag_address
         self.pi = pigpio.pi()
         self._CHANNEL = 1
-        self.DEFAULT_REQUESTS = [
-            #"X_POS","Y_POS","Z_POS",
-            #"X_VEL","Y_VEL","Z_VEL",
-            "X_ACC","Y_ACC","Z_ACC",
-            #"X_DEG","Y_DEG","Z_DEG",
-            "X_DPS","Y_DPS","Z_DPS",
-            "X_MAG","Y_MAG","Z_MAG"
-            ]
 
         self.REQUESTS_REG = {
             #"X_POS":
@@ -26,36 +26,37 @@ class corrected_imu():
             #"X_VEL":
             #"Y_VEL":
             #"Z_VEL":
-            "X_ACC":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
+            "X_ACC":[self._req_N_from_dev,{"registers":[0x29,0x28],"addr":imu_address}],
             #"Y_ACC":
             #"Z_ACC":
             #"X_DEG":
             #"Y_DEG":
             #"Z_DEG":
-            "X_DPS":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
-            "Y_DPS":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
-            "Z_DPS":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
-            "X_MAG":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
-            "Y_MAG":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
-            "Z_MAG":[self._req_N_from_dev,{"registers":[],"addr":imu_address}],
+            "X_DPS":[self._req_N_from_dev,{"registers":[0x23,0x22],"addr":imu_address}],
+            "Y_DPS":[self._req_N_from_dev,{"registers":[0x25,0x24],"addr":imu_address}],
+            "Z_DPS":[self._req_N_from_dev,{"registers":[0x27,0x26],"addr":imu_address}],
+            "X_MAG":[self._req_N_from_dev,{"registers":[0x29,0x28],"addr":mag_address}],
+            "Y_MAG":[self._req_N_from_dev,{"registers":[0x2B,0x2A],"addr":mag_address}],
+            "Z_MAG":[self._req_N_from_dev,{"registers":[0X2D,0X2C],"addr":mag_address}],
         }
     
-    def get_data(data_req = []):
+    def get_data(self,data_req = DEFAULT_REQUESTS):
         if  not isinstance(data_req,list):
             data_req = [data_req]
-        out = dict((req, req[0](**(req[1]))) for req in data_req)
+        out = dict((req, self.REQUESTS_REG[req][0](
+            **(self.REQUESTS_REG[req][1]))) for req in data_req)
         return out
 
     def _req_N_from_dev(self,registers,addr,channel=None):
         data = 0
         for reg in registers:
-            data *= 8
+            data *= 256
             data = self._req8_from_dev(self,reg,addr,channel)
         return data
 
     def _req8_from_dev(self,register,addr,channel=None):
         '''requests the packet from the device'''
-        if channel == None:
+        if channel is None:
             channel=self._CHANNEL
         handle = self.pi.i2c_open(channel,addr)
         data = self.pi.i2c_read_byte_data(handle,register)
