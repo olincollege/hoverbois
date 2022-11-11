8037a65025955ab8fe5ee50ade925980a43bf799
hurricaneJoef <hurricane.joef@gmail.com>
2022-11-10 19:31:01 -0500

    testing new code on pid

--- hoverbotpy/src/hoverbotpy/drivers/pi_pico_pid.py
+++ hoverbotpy/src/hoverbotpy/drivers/pi_pico_pid.py
@@ -196,7 +196,7 @@ def calc_rudder_angle(target_angle, angle_head, angle_vel,
 
     # PD control signal
     signal = (
-        #prop_err * error + 
+        prop_err * error + 
         prop_ddt * angle_vel)
     # Ensure it lies in legal range
     return max(-1, min(1, signal))
