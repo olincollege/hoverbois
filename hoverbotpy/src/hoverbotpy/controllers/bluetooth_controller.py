b2422644147286e9167ba68f3589d1ad298a6208
Devlin Ih <dih@olin.edu>
2022-12-04 23:40:32 -0500

    Add argparse to select driver type

--- hoverbotpy/src/hoverbotpy/controllers/bluetooth_controller.py
+++ hoverbotpy/src/hoverbotpy/controllers/bluetooth_controller.py
@@ -4,9 +4,8 @@ Module implementing control with a bluetooth gamepad.
 
 from evdev import list_devices, InputDevice, ecodes
 
-from hoverbotpy.drivers.driver_dummy import DummyHovercraftDriver
-
 FAN_INCREMENT = 5
+
 
 def find_controller():
     """
@@ -41,7 +40,7 @@ def axis_moved(event, axis, driver):
     axis_relative = axis["value"] - axis["center"]
     if (axis["center"]-axis["deadzone"]
         <= axis_relative
-        <= axis["center"]+axis["deadzone"]):
+            <= axis["center"]+axis["deadzone"]):
         axis_relative = 0
     driver.set_steering_angle(axis_relative)
 
@@ -94,23 +93,55 @@ def estop(driver):
 
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog="BluetoothController",
+        description="Hovercraft controller using bluetooth gamepad.",
+        epilog="Written by Devlin Ih",
+    )
+    parser.add_argument(
+        "driver_type",
+        help=("Type of driver to use. Legal values:\n"
+              "  dummy, dummy_threading, pico, pico_pid"),
+    )
+    args = parser.parse_args()
+    # Wish we were using Python 3.10 for pattern matching.
+    driver_type = args.driver_type
+    if driver_type == "dummy":
+        from hoverbotpy.drivers.driver_dummy import DummyHovercraftDriver
+        driver = DummyHovercraftDriver()
+    elif driver_type == "threading_dummy":
+        from hoverbotpy.drivers.threading_dummy import ThreadingDummy
+        driver = ThreadingDummy()
+        driver.run_loop()
+    elif driver_type == "pico":
+        from hoverbotpy.drivers.pi_pico_simple import SimpleFan
+        driver = SimpleFan()
+    elif driver_type == "pico_pid":
+        from hoverbotpy.drivers.pi_pico_pid import PIDCorrectedFan
+        driver = PIDCorrectedFan()
+        driver.run_loop()
+    else:
+        import sys
+        print(f"Error: {driver_type} is not a valid driver type.")
+        sys.exit(-1)
+
     # Global state
-    driver = DummyHovercraftDriver()
     device = find_controller()
     axis = {
-        "center"   : 0,
-        "value"    : 0,
-        "deadzone" : 100, # Hardcoded for now
+        "center": 0,
+        "value": 0,
+        "deadzone": 100,  # Hardcoded for now
     }
 
     # Button map
     button_map = {
-        ecodes.BTN_SELECT : (recenter, axis),
-        ecodes.BTN_EAST   : (estop, driver),
-        ecodes.BTN_TL2    : (hover_plus, driver),
-        ecodes.BTN_TL     : (hover_minus, driver),
-        ecodes.BTN_TR2    : (throttle_plus, driver),
-        ecodes.BTN_TR     : (throttle_minus, driver),
+        ecodes.BTN_SELECT: (recenter, axis),
+        ecodes.BTN_EAST: (estop, driver),
+        ecodes.BTN_TL2: (hover_plus, driver),
+        ecodes.BTN_TL: (hover_minus, driver),
+        ecodes.BTN_TR2: (throttle_plus, driver),
+        ecodes.BTN_TR: (throttle_minus, driver),
     }
 
     for event in device.read_loop():
@@ -122,3 +153,7 @@ def main():
         # Hardcode the joystick axis because it needs to be done by tomorrow
         if event.type == ecodes.EV_ABS and event.code == ecodes.ABS_X:
             axis_moved(event, axis, driver)
+
+
+if __name__ == "__main__":
+    main()
